91ffff7f4976ebe01733050899b32ea639ea7ebe
orpere <orlando.capoeiraraiz@gmail.com>
2019-07-15 20:40:44 -0300

    users seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 1..10 do
+  test_i = User.new(first_name: "test#{i}", email: "s#{i}@gmail.com", password: 123456)
+  test_i.skip_email_changed_notification!
+  test_i.save
+end
