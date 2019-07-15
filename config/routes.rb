457874cfd32b15fc567d1a3faf4b406a02168309
Fernando E. Silva Jacquier <fesilvajacquier@protonmail.com>
2019-07-15 16:17:01 -0300

    Install Devise. Create User Model. Create basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :gardens do 
+    resources :bookings, only: %i[new create]
+  end
+  
+  resources :bookings, only: %i[edit update show]
+  
+  namespace :admin do
+    resources :gardens, only: %i[index show]
+  end
 end
