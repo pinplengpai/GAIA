6031171c3f00e867d98b33ddeb6ee2eb63af3f4d
orpere <orlando.capoeiraraiz@gmail.com>
2019-07-15 18:29:40 -0300

    validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :garden
+  validates :start_date, :end_date, presence: true, availability: true
+  validate :end_date_after_start_date
+  validates :user_id, presence: true
+  validates :garden_id, presence: true
 end
--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,11 @@
 class Garden < ApplicationRecord
   belongs_to :user
+  validates :image_url, presence: true
+  validates :name, uniqueness: true, presence: true
+  validates :address, uniqueness: true, presence: true
+  validates :description, uniqueness: true, presence: true
+  validates :size, presence: true
+  validates :price, presence: true
+  validates :number_of_guests, presence: true
+  validates :user_id, uniqueness: true, presence: true
 end
