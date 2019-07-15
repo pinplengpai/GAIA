669e1ab4d4c969fc198e2de2e42c6c1753ea67c4
orpere <orlando.capoeiraraiz@gmail.com>
2019-07-15 18:42:02 -0300

    validations fix start_date < end_date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,11 @@
 class Booking < ApplicationRecord
+  def end_date_after_start_date?
+    errors.add :end_date, "must be after start date" if end_date < start_date
+  end
   belongs_to :user
   belongs_to :garden
-  validates :start_date, :end_date, presence: true, availability: true
-  validate :end_date_after_start_date
+  validates :start_date, :end_date, presence: true
+  validate :end_date_after_start_date?
   validates :user_id, presence: true
   validates :garden_id, presence: true
 end
