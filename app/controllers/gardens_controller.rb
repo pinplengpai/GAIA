9c479daf82b86c71454700f2fe0bd2c75edfa7d2
orpere <orlando.capoeiraraiz@gmail.com>
2019-07-17 11:47:37 -0300

    add avatar and landlord views

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,7 +1,7 @@
 class GardensController < ApplicationController
   before_action :set_garden, only: %i[show edit update destroy]
   skip_before_action :authenticate_user!, only: %i[index show]
-  
+
   def index
     @gardens = policy_scope(Garden)
   end
