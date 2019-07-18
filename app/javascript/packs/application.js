5153d6fa7539becdd0964046ef0c069d3c399ea2
Plengpai Ratnajarn <pinplengpai@gmail.com>
2019-07-18 20:13:09 -0300

    try to make the scroll work but doesn't work yet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,8 @@
+import { fixedForm } from '../plugins/init_fix_form';
+fixedForm();
 import "bootstrap";
 import "../plugins/flatpickr";
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
-import { wrap } from '../componets/booking-form';
