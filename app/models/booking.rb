3f1b30d5f32f56afc5b6e6846947ecb9491b33e6
orpere <orlando.capoeiraraiz@gmail.com>
2019-07-18 18:00:38 -0300

    add validation on destroy booking when destroy garden, and created the cards

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
-  STATUS = %w(Pending Confirmed Declined Cancelled)
+  STATUS = %w[Pending Confirmed Declined Cancelled]
   belongs_to :user
   belongs_to :garden
   validates :start_date, :end_date, presence: true
@@ -7,7 +7,6 @@ class Booking < ApplicationRecord
   validates :user_id, presence: true
   validates :garden_id, presence: true
   validates :status, inclusion: { in: STATUS }
-
 
   def end_date_after_start_date?
     errors.add :end_date, "must be after start date" if end_date < start_date
