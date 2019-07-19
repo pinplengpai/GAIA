07b4dfed7cc6408278ed214c6827a2eb323e32ac
orpere <orlando.capoeiraraiz@gmail.com>
2019-07-19 17:00:56 -0300

    fix admin

--- app/controllers/admin/gardens_controller.rb
+++ app/controllers/admin/gardens_controller.rb
@@ -2,6 +2,7 @@ module Admin
   class GardensController < ApplicationController
     def index
       @gardens = current_user.owned_gardens
+      @owned_bookings = current_user.owned_gardens.map(&:bookings).flatten
     end
   end
 end
