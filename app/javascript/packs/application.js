31f78b1a42fc432f5c20386b5d01c5c0f726624c
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2019-07-18 21:12:24 -0300

    work on autocomplete of address field

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import "bootstrap";
 import "../plugins/flatpickr"
+import "../plugins/init_autocomplete"
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
 import { loadDynamicBannerText } from '../components/banner';
